2864089db4d0932c9f441eaeadb81c009b6c2604
Raspreet Singh <raspreet800@gmail.com>
2026-04-02 16:57:58 +0530

    chore (deps,ui): add msw storybook addon to simulate ws connection in stories

--- frontend/.storybook/preview.ts
+++ frontend/.storybook/preview.ts
@@ -1,11 +1,15 @@
 import type { Preview } from '@storybook/react-vite'
+import { initialize, mswLoader } from 'msw-storybook-addon'
+
+initialize({ onUnhandledRequest: 'bypass' })
 
 const preview: Preview = {
+  loaders: [mswLoader],
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
 
