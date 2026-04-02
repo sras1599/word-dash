cc5a3736aa412bbfddba2dd2e5c522ac3dda325d
Raspreet Singh <raspreet800@gmail.com>
2026-04-02 17:03:43 +0530

    ui: add routing for the lobby page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home/Home'
+import { Lobby } from './pages/Lobby/Lobby'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/lobby/:roomCode" element={<Lobby />} />
       </Routes>
     </BrowserRouter>
   )
